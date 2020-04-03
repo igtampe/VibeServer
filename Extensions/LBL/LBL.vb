9bd50c0b6d3c62b80d7001187b6187235d14c0d8
Ignacio Tampe <Igtampe@outlook.com>
2020-04-03 14:36:43 -0400

    Update to LBL 2.0

--- Extensions/LBL/LBL.vb
+++ Extensions/LBL/LBL.vb
@@ -9,7 +9,7 @@ Public Class LBL
     ''' The name of the extension
     ''' </summary>
     Public Const EXTENSION_NAME = "LBL File Transfer"
-    Public Const EXTENSION_VERS = "1.0"
+    Public Const EXTENSION_VERS = "2.0"
 
     ''' <summary>
     ''' Directory on disk to save the received files
@@ -51,8 +51,12 @@ Public Class LBL
                 End If
 
                 Select Case LBLCommand(1).ToUpper
+                    Case "DIR"
+                        Return getDirectory(LBLCommand(2))
                     Case "OPEN"
                         Return Open(LBLCommand(2))
+                    Case "OPENGET"
+                        Return OpenGet(LBLCommand(2))
                     Case "TRANSFER"
                         If LBLCommand.Count >= 4 Then
                             Return Transfer(LBLCommand(2), LBLCommand(3))
@@ -74,11 +78,16 @@ Public Class LBL
         End Try
     End Function
 
-    Private Function Open(Filename As String) As String
-        'Start a transfer
-        ToConsole("Attempting to start a transfer for file " & Filename & "...")
+    Private Function getDirectory(Path As String) As String
+        Dim AllFiles As String() = IO.Directory.GetFiles(WebDirectory & Path)
+        For index = 0 To AllFiles.Length - 1
+            AllFiles(index) = AllFiles(index).Replace(WebDirectory & "\", "")
+        Next
+        Return String.Join(",", AllFiles)
+    End Function
 
-        Dim NFTID = ""
+    Private Function GetNewTransactionID() As Integer
+        Dim NFTID As String
 
         Do
             'Generate a transfer ID
@@ -92,12 +101,44 @@ Public Class LBL
             'Check that it isn't in use
         Loop While Not IsNothing(GetTransfer(NFTID))
 
+        Return NFTID
+
+    End Function
+
+    Private Function OpenGet(Filename As String)
+        'Start a get file transfer
+        ToConsole("Attempting to start a new Get File transfer")
+
+        'Security cosito
+        Filename = Filename.Replace("..\", "")
+
+        If Not File.Exists(WebDirectory & "\" & Filename) Then
+            ToConsole("File " & Filename & " Could not be found", ConsoleColor.DarkRed)
+            Return "NE"
+        End If
+
+        Return InitializeTransfer(GetNewTransactionID(), Filename, LBLTransfer.TransferType.Send)
+
+    End Function
+
+    Private Function Open(Filename As String) As String
+        'Start a transfer
+        ToConsole("Attempting to start a transfer for file " & Filename & "...")
+        Return InitializeTransfer(GetNewTransactionID(), Filename.Replace("..\", ""), LBLTransfer.TransferType.Receive)
+    End Function
+
+    Private Function InitializeTransfer(NFTID As Integer, Filename As String, Transfer As LBLTransfer.TransferType)
         ToConsole("Attempting to initialize transfer " & NFTID)
 
         Try
-            ActiveTransfers.Add(New LBLTransfer(NFTID, Filename, WebDirectory))
+            Dim NewTransfer As LBLTransfer = New LBLTransfer(NFTID, Filename, WebDirectory, Transfer)
+            ActiveTransfers.Add(NewTransfer)
             ToConsole("Success! Transfer " & NFTID & " of file " & Filename & " has been initiaized. Available to receive data.", ConsoleColor.Green)
-            Return NFTID
+            If Transfer = LBLTransfer.TransferType.Send Then
+                Return NFTID & ":" & NewTransfer.AllLines.Count
+            Else
+                Return NFTID
+            End If
         Catch ex As Exception
             ErrorToConsole("Unable to add the new transfer to the list of active transfers", ex)
             Return "E"
@@ -109,19 +150,47 @@ Public Class LBL
         Dim TheTransfer As LBLTransfer = GetTransfer(TransferID)
 
         If Not IsNothing(TheTransfer) Then
-            Try
-                TheTransfer.Append(TransferData)
-                ToConsole("Successfully appended the received data to Transfer " & TransferID, ConsoleColor.Green)
-                Return "OK"
-            Catch ex As Exception
-                ErrorToConsole("Unable to append data to the Transfer's file", ex)
-                Return "E"
-            End Try
+            Select Case TheTransfer.Type
+                Case LBLTransfer.TransferType.Receive
+                    Return TransferReceive(TheTransfer, TransferData)
+                Case LBLTransfer.TransferType.Send
+                    Return TransferSend(TheTransfer)
+                Case Else
+                    ToConsole("Uh, the transfer didn't have a TransferType. This shouldn't be able to happen at all", ConsoleColor.DarkRed)
+                    Return "E"
+            End Select
         Else
             ToConsole("Could not find Transfer " & TransferID, ConsoleColor.DarkRed)
             Return "E"
         End If
 
+    End Function
+
+    Private Function TransferReceive(Transfer As LBLTransfer, TransferData As String) As String
+        Try
+            Transfer.Append(TransferData)
+            ToConsole("Successfully appended the received data to Transfer " & Transfer.ID, ConsoleColor.Green)
+            Return "OK"
+        Catch ex As Exception
+            ErrorToConsole("Unable to append data to the Transfer's file", ex)
+            Return "E"
+        End Try
+    End Function
+
+    Private Function TransferSend(Transfer As LBLTransfer)
+        Try
+            ToConsole("Getting next line " & Transfer.ID, ConsoleColor.Green)
+            Return Transfer.GetLine()
+        Catch ex As Exception
+            If ex.Equals(LBLTransfer.LBLAlreadyDoneException) Then
+                ErrorToConsole("Reached EOF. PLease close this file!", ex)
+                Return "LBL.PLSCLOSE"
+            Else
+                ErrorToConsole("Unable to get data to the Transfer's file", ex)
+                Return "E"
+            End If
+
+        End Try
     End Function
 
     Private Function Close(TransferID As Integer)
--- Extensions/LBL/LBLTransfer.vb
+++ Extensions/LBL/LBLTransfer.vb
@@ -1,9 +1,18 @@
-﻿Imports Utils
+﻿Imports System.Collections
+Imports Utils
 Public Class LBLTransfer
 
     Public Filename As String
     Public ID As Integer
     Public Directory As String
+    Public Type As TransferType
+    Public AllLines As Queue
+    Public Enum TransferType As Integer
+        Receive
+        Send
+    End Enum
+
+    Public Shared LBLAlreadyDoneException As Exception = New Exception("The File is Done")
 
     ''' <summary>
     ''' Create a new transfer with the specified ID and to the specified filename
@@ -11,11 +20,23 @@ Public Class LBLTransfer
     ''' <param name="ID">ID of this transfer</param>
     ''' <param name="Filename">Name of the file (IE Me.csv)</param>
     ''' <param name="Directory">Directory to save it (AS "C:\IIS\FILES")</param>
-    Public Sub New(ID As Integer, Filename As String, Directory As String)
+    Public Sub New(ID As Integer, Filename As String, Directory As String, Type As TransferType)
         Me.ID = ID
         Me.Filename = Filename
         Me.Directory = Directory
-        If IO.File.Exists(Directory & "\" & Filename) Then IO.File.Delete(Directory & "\" & Filename)
+        Me.Type = Type
+        If Type = TransferType.Send Then
+            'Open and read all the contents of the file. Add them to a queue to send
+            FileOpen(1, Directory & "\" & Filename, OpenMode.Input)
+            AllLines = New Queue
+            While Not EOF(1)
+                AllLines.Enqueue(LineInput(1))
+            End While
+            FileClose(1)
+        Else
+            If IO.File.Exists(Directory & "\" & Filename) Then IO.File.Delete(Directory & "\" & Filename)
+        End If
+
     End Sub
 
     ''' <summary>
@@ -23,12 +44,22 @@ Public Class LBLTransfer
     ''' </summary>
     ''' <param name="What"></param>
     Public Sub Append(What As String)
+        If Not Type = TransferType.Receive Then Throw New ArgumentException()
         AddToFile(Directory & "\" & Filename, What)
     End Sub
 
+    Public Function GetLine() As String
+        If Not Type = TransferType.Send Then Throw New ArgumentException()
+        If AllLines.Count = 0 Then Throw LBLAlreadyDoneException
+        Return AllLines.Dequeue()
+    End Function
+
     Public Sub Close()
+        If Type = TransferType.Send Then AllLines = Nothing
+
         ID = Nothing
         Filename = Nothing
+
     End Sub
 
     Public Overrides Function Equals(obj As Object) As Boolean
@@ -42,5 +73,7 @@ Public Class LBLTransfer
     Public Overrides Function ToString() As String
         Return Filename & " (ID: " & ID & ")"
     End Function
+
+
 
 End Class
